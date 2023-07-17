b84027e63a1098e71dc7a310ef43e7082d11cf01
kylew1004 <kylew1004@gmail.com>
2023-07-17 17:39:31 +0900

    jpa 시작하기

---
+++ inflearn-JPA/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,38 @@
+package hellojpa;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+
+import java.util.List;
+
+import static javax.persistence.Persistence.createEntityManagerFactory;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            // Member findMember = em.find(Member.class, 1L);
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
